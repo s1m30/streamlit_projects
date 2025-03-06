b07dfb1aeaca05f6f56fa320fd8d79dd958da170
madscientist <fikunyin.adebola@gmail.com>
2025-03-06 01:58:07 +0100

    Eleventh commit

--- Corporate_Law_RAG/app.py
+++ Corporate_Law_RAG/app.py
@@ -1,19 +1,108 @@
 import streamlit as st
 from setup import config_options, init_messages
-from utils import answer_question
 from snowflake.snowpark import Session
 from snowflake.core import Root # requires snowflake>=0.8.0
 from setup import get_parameters
+from snowflake.cortex import complete
+
+CORTEX_SEARCH_DATABASE = "STREAMLIT_APP"
+CORTEX_SEARCH_SCHEMA = "DATA_SCHEMA"
+CORTEX_SEARCH_SERVICE= "LEGAL_CHECKER"
+### Default Values
+NUM_CHUNKS = 5# Num-chunks provided as context. Play with this to check how it affects your accuracy
+slide_window = 7 # how many last conversations to remember. This is the slide window.
+# Ensure credentials are stored in st.session_state and reuse session
+if 'session' not in st.session_state:
+    st.session_state.session = Session.builder.configs(get_parameters(st.secrets["ACCOUNT"],st.secrets["USER"],st.secrets["PASSWORD"])).create()
+root=Root(st.session_state.session)
+# columns to query in the service
+COLUMNS = [
+    "chunk",
+    "category"]
+
+
+#Describe Prompt
+def create_prompt (myquestion):
+    chat_history = get_chat_history()
+    if chat_history != []: #There is chat_history, so not first question
+        question_summary = summarize_question_with_history(chat_history, myquestion)
+        prompt_context =  get_similar_chunks_search_service(question_summary)
+    else:
+        prompt_context = get_similar_chunks_search_service(myquestion) #First question when using history
+  
+    prompt = f"""
+           You are an expert company law advisor that extracts information from the CONTEXT on Nigerian company law 
+           provided between <context> and </context> tags to help the user.
+           You offer a chat experience considering the information included in the CHAT HISTORY
+           provided between <chat_history> and </chat_history> tags..
+           When ansering the question contained between <question> and </question> tags
+           be concise and do not hallucinate. 
+           Do not mention the CONTEXT used in your answer.
+           Do not mention the CHAT HISTORY used in your asnwer.
+
+           Only answer the question if you can extract it from the CONTEXT provideed.
+           
+           <chat_history>
+           {chat_history}
+           </chat_history>
+           <context>          
+           {prompt_context}
+           </context>
+           <question>  
+           {myquestion}
+           </question>
+           Answer: 
+           """
+    return prompt
+
+
+def answer_question(myquestion):
+    prompt =create_prompt (myquestion)
+    response = complete(model=st.session_state.model_name, prompt=prompt,session=st.session_state.session)
+    return response
+
+
+def get_similar_chunks_search_service(query):
+    #Define Search Service 
+    svc = root.databases[CORTEX_SEARCH_DATABASE].schemas[CORTEX_SEARCH_SCHEMA].cortex_search_services[CORTEX_SEARCH_SERVICE]
+    if st.session_state.category_value == "ALL":
+        response = svc.search(query, COLUMNS, limit=NUM_CHUNKS)
+    elif st.session_state.category_value != "ALL":
+        filter_obj = {"@eq": {"category": st.session_state.category_value} }
+        response = svc.search(query, COLUMNS, filter=filter_obj, limit=NUM_CHUNKS)
+    st.sidebar.json(response.json())
+    return response.json()
+
+#Get the history from the st.session_stage.messages according to the slide window parameter
+def get_chat_history():
+    chat_history = []
+    start_index = max(0, len(st.session_state.messages) - slide_window)
+    for i in range (start_index , len(st.session_state.messages) -1):
+         chat_history.append(st.session_state.messages[i])
+
+    return chat_history
+
+# To get the right context, use the LLM to first summarize the previous conversation
+# This will be used to get embeddings and find similar chunks in the docs for context
+def summarize_question_with_history(chat_history, question):
+    prompt = f"""
+        Based on the chat history below and the question, generate a query that extend the question
+        with the chat history provided. The query should be in natual language. 
+        Answer with only the query. Do not add any explanation.
+        
+        <chat_history>
+        {str(chat_history)}
+        </chat_history>
+        <question>
+        {question}
+        </question>
+        """
+    print("This is the prompt",prompt)
+    summary = complete(st.session_state.model_name, prompt)   
+    summary = summary.replace("'", "")
+    return summary
+
 def main():
-    # Ensure credentials are stored in st.session_state and reuse session
-    if 'session' not in st.session_state:
-        st.session_state.session = Session.builder.configs(get_parameters(st.secrets["ACCOUNT"],st.secrets["USER"],st.secrets["PASSWORD"])).create()
-    elif not isinstance(st.session_state.session, Session): # Ensure it's a valid session object, in case of serialization issues
-        st.session_state.session = Session.builder.configs(get_parameters(st.secrets["ACCOUNT"],st.secrets["USER"],st.secrets["PASSWORD"])).create()
-
-    if 'root' not in st.session_state:
-        st.session_state.root = Root(st.session_state.session)
-
     st.title("📚💡 Smart Company Law Advisor ")
     st.write(
         "Get instant insights on Nigerian company law with AI, based on the Companies and Allied Matters Act (CAMA) 2020. "
